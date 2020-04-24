5f9ba7a0d092e186869ad7d4cea4714fa173a1f1
Timothy Scott <scottti@student.ethz.ch>
2020-04-24 16:17:15 +0200

    Added supermegabot configuration

---
+++ vil_fusion/src/imgflip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+from __future__ import division, print_function
+
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+node = rospy.init_node("imgflip")
+
+bridge = CvBridge()
+pub = rospy.Publisher("/cam0/image_flipped", Image)
+
+
+def img_callback(img):
+    cv_img = bridge.imgmsg_to_cv2(img, desired_encoding="passthrough")
+    h, w = cv_img.shape[0:2]
+    m = cv2.getRotationMatrix2D((w / 2, h / 2), 180, 1)
+    rotated = cv2.warpAffine(cv_img, m, (w, h))
+    new_img = bridge.cv2_to_imgmsg(rotated)
+    new_img.header = img.header
+    pub.publish(new_img)
+
+
+sub = rospy.Subscriber("/cam0/image_rect", Image, callback=img_callback)
+
+if __name__ == "__main__":
+    rospy.spin()
